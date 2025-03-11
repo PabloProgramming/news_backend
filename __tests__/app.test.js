bc0d6c9e95e08059b4374ee79769b34d9dfbf99c
PabloProgramming <pmontalvoster@gmail.com>
2025-03-11 12:37:06 +0000

    test: add error handling tests for invalid article_id, empty comments and query parameters
    - Added test for 404 response when article_id is not found.
    - Added test for 400 response when article_id is not a number.
    - Added test for 400 response when invalid query sort_by values are provided.
    - Added test for 400 response when invalid query order values are provided.
    - Added test for 404 response when no comments are associated with the provided article_id.

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -131,7 +131,7 @@ describe("GET: /api/articles", () => {
 
 describe("GET: /api/articles:article:id/comments", () => {
   test("Returns a 200 OK status when an comments are fetched successfully", async () => {
-    await request(app).get("/api/articles/2/comments").expect(200);
+    await request(app).get("/api/articles/3/comments").expect(200);
   });
 
   test("Returns an array of comments when a valid article_id is provided", async () => {
@@ -150,17 +150,52 @@ describe("GET: /api/articles:article:id/comments", () => {
       expect(comment.article_id).toBe(3);
     });
   });
-    test("Returns an array of comments by article_id sorted by date in asc order", async () => {
+  test("Returns an array of comments by article_id sorted by date in asc order", async () => {
+    const {
+      body: {comments},
+    } = await request(app)
+      .get("/api/articles/3/comments?order=asc")
+      .expect(200);
+    expect(comments.length).not.toBe(0);
+    expect(comments).toBeSortedBy("created_at", {ascending: true});
+  });
+  describe("💥 Error handling tests", () => {
+    test("Returns 404 when the article_id is not found", async () => {
       const {
-        body: {comments},
-      } = await request(app)
-        .get("/api/articles/3/comments?order=asc")
-          .expect(200);
-      expect(comments.length).not.toBe(0);
-      expect(comments).toBeSortedBy("created_at", {ascending: true});
+        body: {msg},
+      } = await request(app).get(`/api/articles/123123/comments`).expect(404);
+      expect(msg).toBe("Not Found");
     });
+    test("Returns 400 when the article_id is not a number", async () => {
+      const {
+        body: {msg},
+      } = await request(app)
+        .get(`/api/articles/badrequest/comments`)
+        .expect(400);
+      expect(msg).toBe("Bad Request");
+    });
+    test("Returns 400 when provided with wrong query sort_by values", async () => {
+      const {
+        body: {msg},
+      } = await request(app)
+        .get("/api/articles/3/comments?sort_by=invalid_column")
+        .expect(400);
+      expect(msg).toBe("Bad Request");
+    });
+    test("Returns 400 when provided with wrong query order values", async () => {
+      const {
+        body: {msg},
+      } = await request(app)
+        .get("/api/articles/3/comments?sort_by=created_at&order=invalid")
+        .expect(400);
+      expect(msg).toBe("Bad Request");
+    });
+    test("Returns 404 when there are not comments associated with provided article_id", async () => {
+      const {
+        body: {msg},
+      } = await request(app).get("/api/articles/2/comments").expect(404);
+      expect(msg).toBe("Not Found");
+    });
+  });
 });
-
-
-
 
--- models/commentModels.js
+++ models/commentModels.js
@@ -8,8 +8,23 @@ const selectCommentsByArticleId = async (
   sort_by = "created_at",
   order = "desc"
 ) => {
+  if (!allowedSortingQueries.includes(sort_by)) {
+    return Promise.reject({
+      status: 400,
+      msg: "Bad Request",
+    });
+  }
+
+  if (!allowedOrderQueries.includes(order)) {
+    return Promise.reject({
+      status: 400,
+      msg: "Bad Request",
+    });
+  }
+
   let queryStr = `SELECT * FROM comments WHERE article_id = $1`;
   let queryParams = [];
+
   if (article_id) {
     queryParams.push(article_id);
   }
@@ -23,8 +38,18 @@ const selectCommentsByArticleId = async (
 
   const {rows} = await db.query(queryStr, queryParams);
   const comments = rows;
+  if (comments.length === 0) {
+     return Promise.reject({
+       status: 404,
+       msg: "Not Found",
+     });
+  }
   return comments;
 };
 
 module.exports = selectCommentsByArticleId;
+
+
+
+
 
